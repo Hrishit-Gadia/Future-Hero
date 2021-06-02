ab5a37b62d957c7f5d790b084886b78279e8bd6c
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-06-02 16:53:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var Canvas = new fabric.Canvas('The-Canvas');
-var Height = 150;
-var Width = 50;
+var Size = 50;
 var PlayerObject;
 var ObjectImage;
-var PlayerX;
-var PlayerY;
+var PlayerX = 200;
+var PlayerY = 200;
+var Key;
 
 function Player_Update() {
     fabric.Image.fromURL("player.png", function (Img) {
@@ -26,8 +26,8 @@ function Player_Update() {
 function Part_Update(GetImage) {
     fabric.Image.fromURL(GetImage, function (Img) {
         BlockObject = Img;
-        BlockObject.scaleToWidth(Width);
-        BlockObject.scaleToHeight(Height);
+        BlockObject.scaleToWidth(Size);
+        BlockObject.scaleToHeight(Size);
         BlockObject.set(
             {
                 top: PlayerY,
@@ -37,4 +37,113 @@ function Part_Update(GetImage) {
         Canvas.add(BlockObject);
     }
     )
+}
+
+window.addEventListener("keydown" , KeyCheck);
+
+function KeyCheck(e) {
+Key = e.keyCode;
+
+if (e.shiftKey == true && Key == 84) {
+    console.log("Shift + T");
+    Size = Size + 10;
+    document.getElementById("Size").innerHTML = Size;
+}
+if (e.shiftKey == true && Key == 85) {
+    console.log("Shift + U");
+    Size = Size - 10;
+    document.getElementById("Size").innerHTML = Size;
+}
+if ( e.shiftKey == true && Key == 65) {
+    console.log("Iron Face");
+    Part_Update("ironman_face.png");
+}
+if ( e.shiftKey == true && Key == 66) {
+    console.log("Thor Face");
+    Part_Update("thor_face.png");
+}
+if ( e.shiftKey == true && Key == 67) {
+    console.log("Hulk face");
+    Part_Update("hulk_face.png");
+}
+if ( e.shiftKey == true && Key == 68) {
+    console.log("spider face");
+    Part_Update("spiderman_face.png");
+}
+if ( e.shiftKey == true && Key == 69) {
+    console.log("Iron face");
+    Part_Update("ironman_body.png");
+}
+if ( e.shiftKey == true && Key == 70) {
+    console.log("hulk face");
+    Part_Update("hulkd_body.png");
+}
+if ( e.shiftKey == true && Key == 71) {
+    console.log("Spider face");
+    Part_Update("spiderman_body.png");
+}
+if ( e.shiftKey == true && Key == 72) {
+    console.log("Iron Right Hand");
+    Part_Update("ironman_right_hand.png");
+}
+if ( e.shiftKey == true && Key == 73) {
+    console.log("Thor Right Hand");
+    Part_Update("thor_right_hand.png");
+}
+if ( e.shiftKey == true && Key == 74) {
+    console.log("Hulk Right Hand");
+    Part_Update("hulk_right_hand.png");
+}
+if ( e.shiftKey == true && Key == 75) {
+    console.log("Spider Right Hand");
+    Part_Update("spiderman_right_hand.png");
+}
+if ( e.shiftKey == true && Key == 76) {
+    console.log("Spider Left Hand");
+    Part_Update("spiderman_left_hand.png");
+}
+if ( e.shiftKey == true && Key == 77) {
+    console.log("Thor Left Hand");
+    Part_Update("thor_left_hand.png");
+}
+if ( e.shiftKey == true && Key == 78) {
+    console.log("Iron Left Hand");
+    Part_Update("ironman_left_hand.png");
+}
+if ( e.shiftKey == true && Key == 79) {
+    console.log("Hulk Left Hand");
+    Part_Update("hulk_left_hand .png");
+}
+if ( e.shiftKey == true && Key == 80) {
+    console.log("Captain Left Hand");
+    Part_Update("captain_america_left_hand.png");
+}
+if ( e.shiftKey == true && Key == 81) {
+    console.log("Hulk Legs");
+    Part_Update("hulk_legs.png");
+}
+if ( e.shiftKey == true && Key == 82) {
+    console.log("Iron Legs");
+    Part_Update("ironman_legs.png");
+}
+if ( e.shiftKey == true && Key == 83) {
+    console.log("Spider Legs");
+    Part_Update("spiderman_legs.png");
+}
+if ( Key == 38){
+    console.log("Up")
+    Up();
+}
+if ( Key == 39){
+    console.log("Right")
+    Right();
+}
+if ( Key == 40){
+    console.log("Down")
+    Down();
+}
+if ( Key == 37){
+    console.log("Left")
+    Left();
+}
 }
